af3714d6d6f03e10d822960ba87badfa4a1aa92c
aligulfaamstudent <90401861+aligulfaamstudent@users.noreply.github.com>
2022-03-22 11:27:14 -0400

    first commit

---
+++ python pencil art/pencil_art.py
@@ -0,0 +1,36 @@
+pencil_art = '''
+        l
+       l l
+      l   l
+     l     l   
+    l       l
+   l         l
+  l           l
+ l`i`i`i`i`i`i`l
+l               l
+l               l
+l               l
+l               l
+l               l
+l     IIII      l
+l   II    II    l
+l        II     l
+l       II      l
+l      II       l
+l     II        l
+l    IIIIIII    l
+l               l
+l               l
+l               l
+l               l
+l               l
+l i i i i i i i l
+l i i i i i i i l
+l i i i i i i i l
+ @ @ @ @ @ @ @ @
+ @ @ @ @ @ @ @ @
+ @ @ @ @ @ @ @ @
+ @ @ @ @ @ @ @ @
+'''
+print(pencil_art)
+
---
+++ python pencil art/print_example.py
@@ -0,0 +1,49 @@
+print("Hello Ninja")
+### hi this is a comment.
+#! Download "Better Coments" to stylize your comments
+#? Awesome Colors
+your_name = "Captain Awesome"
+print(your_name)
+
+my_story = '''
+Hello
+    There 
+        Look at these
+                            tabs
+'''
+print(my_story)
+age = 157
+retirment_age = 62
+print(age - retirment_age)
+
+tempurature = 70.5
+#! Addition 
+x = 3
+y = 11
+
+print(x+y)
+#!Subtrction
+print(x-y)
+#! Multiplication
+print(x*y)
+#! Division 
+print(x/y)
+#! Modulo - Remainder
+print(y%x)
+#! Exponents
+print(y**x)
+#? Change Number/Value
+x = 5
+print(x**y)
+
+#? Concatenation 
+print(your_name + my_story + str(x))
+
+
+#! +=
+x+=3
+print(y-x)
+
+favorite_number = input("Please enter a number: ")
+favorite_number = int(favorite_number)
+print(f"x + y + {str(favorite_number)} = "+ str(x+y+favorite_number))
